9e5442be229e90bcbf1c3f95b88d44f9916e3d6a
khapiton04 <138171279+khapiton04@users.noreply.github.com>
2023-06-30 12:58:53 +0300

    main.rs

--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
     for num in 1..=100 {
-        if num % 3 == 0 {
+        if num % 15 == 0 {
             println!("FizzBuzz");
         } else if num % 5 == 0 {
             println!("Fizz");
-        } else if num % 15 == 0 {
+        } else if num % 3 == 0 {
             println!("Buzz");
         } else {
             println!("{}", num);
